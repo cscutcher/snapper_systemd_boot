17a72f1784756be8114793586602c9833f29f3b3
Chris Scutcher <chris@scutcher.uk>
2018-04-04 20:12:07 +0100

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name="snapper_systemd_boot",
-    version="0.1",
+    description="Generate systemd-boot entries from snapper btrfs snapshots.",
+    version="0.1.0-alpha",
     packages=find_packages(),
     install_requires=[
         "reprutils",
@@ -15,5 +16,20 @@ setup(
         "console_scripts": [
             "snapper-systemd-boot = snapper_systemd_boot.cli:main",
         ]
-    }
+    },
+    python_requires=">=3.6",
+    license="Apache-2.0",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: System :: Archiving :: Backup",
+        "Topic :: System :: Systems Administration",
+    ],
+    keywords="systemd-boot snapper btrfs",
 )
